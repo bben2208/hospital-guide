0a88278e9d815402d5aebac3dbbab4538ffd794e
Benjamin Bognar <benjaminbognar@MacBook-Pro-2.local>
2025-07-14 19:31:07 +0100

    netfligy 404

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        {/* <Route path="/" element={<Login />} /> */}
+       
         <Route path="/hospitals" element={<HospitalList />} />
         <Route path="/search/:hospitalId" element={<SearchPage />} />
       </Routes>
