6503cbb32050451523d75274f87d03ba363805b2
Benjamin Bognar <benjaminbognar@MacBook-Pro-2.local>
2025-07-17 08:18:53 +0100

    deployment fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,10 @@
-// vite.config.js
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite';           // ✅ this line was missing!
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5174, // 👈 your custom port
+    port: 5174,
     proxy: {
       '/api': {
         target: 'http://localhost:5500',
